2c424e30c07b980d47e05721d96494efb0aec3ba
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-12-20 14:58:32 +0000

    Added Payroll and Report

--- src/HR_System/Developer.java
+++ src/HR_System/Developer.java
@@ -7,16 +7,22 @@ public class Developer extends Employee {
   private int hours;
 
   private static int totalDevelopers = 0;
+  private static int totalPayroll = 0;
 
   public Developer(String fname, String lname, String address, String med, int sal, int theHours){
     super(fname, lname, address, med, sal);
     hours = theHours;
 
     totalDevelopers++;
+    totalPayroll = totalPayroll + sal;
   }
 
   public static int getTotalDevelopers(){
     return totalDevelopers;
+  }
+
+  public static int getTotalPayroll(){
+    return totalPayroll;
   }
 
   public String toString(){
--- src/HR_System/Workplace.java
+++ src/HR_System/Workplace.java
@@ -7,34 +7,40 @@ public class Workplace {
 
   public static void main(String[] args) throws IOException{
 
-    String choice;
+    String choice = "0";
     String username;
     String password;
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
     System.out.println(" ");
     System.out.println("Welcome to the NL Human Resources Information System 3000");
-    System.out.println("Please enter your username and password: ");
+    System.out.println("Please enter your username (First Name) and password (Last Name)");
     System.out.print("Username: ");
     username = keyboard.readLine();
     System.out.print("Password: ");
     password = keyboard.readLine();
 
-    System.out.println("\nSelect one of the options below to view their information: ");
-    System.out.println("1 - CEO");
-    System.out.println("2 - Software Development Managers");
-    System.out.println("3 - Software Developers");
+    if (username.equalsIgnoreCase("Eric") && password.equalsIgnoreCase("Fabroa")) {
+      System.out.println("\nSelect one of the options: ");
+      System.out.println("1 - CEO");
+      System.out.println("2 - Software Development Managers");
+      System.out.println("3 - Software Developers");
+      System.out.println("4 - HR Report");
 
-		choice = keyboard.readLine();
+      choice = keyboard.readLine();
+    } else {
+      System.out.println("Incorrect Username/Password. Please try again later.");
+    }
 
-    while (choice.equals("1") || choice.equals("3") || choice.equals("3")) {
+
+    while (choice.equals("1") || choice.equals("2") || choice.equals("3")) {
 
       if (choice.equals("1")) {
         CEO Daenerys;
         Daenerys = new CEO("Daenerys", "Targaryen", "34 Iron Throne Rd.", "Peanut Allergy", 5000000);
         System.out.println(Daenerys);
         
-        System.out.println("Please choose again");
+        System.out.println("\nSelect one of the options again or enter exit");
 		    choice = keyboard.readLine();
       }
 
@@ -43,7 +49,7 @@ public class Workplace {
         Jon = new Manager("Jon", "Snow", "23 The Wall Rd.", "Asthma", 150000, "Music App");
         System.out.println(Jon);
         
-        System.out.println("Please choose again");
+        System.out.println("\nSelect one of the options again or enter exit");
 		    choice = keyboard.readLine();
       }
 
@@ -56,11 +62,28 @@ public class Workplace {
         Tyrion = new Developer("Tyrion", "Lannister", "75 Casterly Street", "Shellfish Allergy", 70000, 88);
         System.out.println(Tyrion);
 
-        System.out.println("Total Number of Developers: " + Developer.getTotalDevelopers());
+        System.out.println("Enter 'yes' if you would like to add a new Developer");
+        String addDev = keyboard.readLine();
 
-        System.out.println("Please choose again");
+        if (addDev.equals("yes")) {
+          Developer newDev;
+          String fName = keyboard.readLine();
+          String lName = keyboard.readLine();
+          String address = keyboard.readLine();
+          String medCondition = keyboard.readLine();
+          newDev = new Developer(fName, lName, address, medCondition, 70000, 0);
+          System.out.println(newDev);
+        }
+
+        System.out.println("\nSelect one of the options again or enter exit");
 		    choice = keyboard.readLine();
+      } 
+
+      if (choice.equals("4")) {
+        System.out.println("Total Number of Developers: " + Developer.getTotalDevelopers());
+        System.out.println("Total Payroll: $" + Developer.getTotalPayroll());
       }
+
 
 
     }
